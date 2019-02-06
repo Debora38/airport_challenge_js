fcfef96f02cc0748cf52aeb83646fe8974aaf237
Debora <debora.piu92@gmail.com>
2019-02-06 18:27:16 +0000

    simple land and take off methods in airport

--- spec/airport_spec.js
+++ spec/airport_spec.js
@@ -1,10 +1,21 @@
 describe("Airport", function() {
   var airport;
+  var plane;
 
   beforeEach(function() {
+    plane = new Plane();
     airport = new Airport();
   });
 
-  
+  it("should land a plane", function() {
+    airport.land(plane);
+    expect(airport.planes()).toContain(plane);
+  });
+
+  it("should take off a plane", function() {
+    airport.land(plane);
+    airport.takeOff(plane);
+    expect(airport.planes()).not.toContain(plane);
+  });
 
 });
--- src/airport.js
+++ src/airport.js
@@ -1,3 +1,17 @@
-var Airport = function() {
+function Airport() {
+  this._planes = [];
+};
 
+Airport.prototype.planes = function() {
+  return this._planes;
 }
+
+Airport.prototype.land = function(plane) {
+  plane.land()
+  this._planes.push(plane)
+};
+
+Airport.prototype.takeOff = function(plane) {
+  plane.takesOff()
+  this._planes.splice( this._planes.indexOf(plane), 1 );
+};
